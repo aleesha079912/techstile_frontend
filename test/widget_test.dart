efd2797e5271f320b9a2dbd08a62544bc3fb8d29
irhagck <alimuhammadsanaullah@gmail.com>
2026-06-13 13:19:01 +0500

    Profile

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,15 +4,13 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:techstile_frontend/main.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ServEaseApp());
-    expect(find.text('ServEase'), findsOneWidget);
+    await tester.pumpWidget(const TECHstile());
+    expect(find.text('Techstile'), findsOneWidget);
   });
 }
